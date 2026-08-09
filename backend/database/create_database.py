b6c1f59178d3a0a73bcf7cf905489935316cf125
Sakshi Maholiya <apurvamaholiya@gmail.com>
2026-08-10 01:25:23 +0530

    Fix import path in create_database.py

--- backend/database/create_database.py
+++ backend/database/create_database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-from backend.config.config import Config
+from backend.config import Config
 
 engine = create_engine(
     Config.DATABASE_URL,
