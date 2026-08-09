8d6288644ebbbaad77d3a5bccdffe93ccb7ccc84
Sakshi Maholiya <apurvamaholiya@gmail.com>
2026-08-09 21:37:10 +0530

    Update the config file

--- backend/database/create_database.py
+++ backend/database/create_database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-from backend.config import Config
+from backend.config.config import Config
 
 engine = create_engine(
     Config.DATABASE_URL,
